9639be8e3c29eaf5a698e3ea47c92e7bba9fa68f
vacherkasskiy <90340909+vacherkasskiy@users.noreply.github.com>
2022-12-05 04:34:36 +0300

    Add files via upload

---
+++ HW1/src/Finder.java
@@ -0,0 +1,283 @@
+import java.util.ArrayList;
+
+public class Finder {
+    private void deleteLastKElements(int counter, ArrayList<Cell> cells) {
+        for (int k = 1; k <= counter; k++) {
+            cells.remove(cells.size() - 1);
+        }
+    }
+    private void colorHorizontals(Cell[][] _fieldArray, int i, int j) {
+        Colors playerColor = _fieldArray[i][j].getColor();
+        ArrayList<Cell> cellsToColor = new ArrayList<Cell>();
+        int counter = 0;
+        for (int n = i - 1; n >= 0; n--) {
+            if (!_fieldArray[n][j].isFree()) {
+                if (_fieldArray[n][j].getColor() != playerColor) {
+                    cellsToColor.add(_fieldArray[n][j]);
+                    ++counter;
+                    if (n == 0) { deleteLastKElements(counter, cellsToColor); }
+                } else {
+                    break;
+                }
+            } else {
+                deleteLastKElements(counter, cellsToColor);
+                break;
+            }
+        }
+        counter = 0;
+        for (int n = i + 1; n < 8; n++) {
+            if (!_fieldArray[n][j].isFree()) {
+                if (_fieldArray[n][j].getColor() != playerColor) {
+                    cellsToColor.add(_fieldArray[n][j]);
+                    ++counter;
+                    if (n == 7) { deleteLastKElements(counter, cellsToColor); }
+                } else {
+                    break;
+                }
+            } else {
+                deleteLastKElements(counter, cellsToColor);
+                break;
+            }
+        }
+        counter = 0;
+        for (int m = j - 1; m >= 0; m--) {
+            if (!_fieldArray[i][m].isFree()) {
+                if (_fieldArray[i][m].getColor() != playerColor) {
+                    cellsToColor.add(_fieldArray[i][m]);
+                    ++counter;
+                    if (m == 0) { deleteLastKElements(counter, cellsToColor); }
+                } else {
+                    break;
+                }
+            } else {
+                deleteLastKElements(counter, cellsToColor);
+                break;
+            }
+        }
+        counter = 0;
+        for (int m = j + 1; m < 8; m++) {
+            if (!_fieldArray[i][m].isFree()) {
+                if (_fieldArray[i][m].getColor() != playerColor) {
+                    cellsToColor.add(_fieldArray[i][m]);
+                    ++counter;
+                    if (m == 7) { deleteLastKElements(counter, cellsToColor); }
+                } else {
+                    break;
+                }
+            } else {
+                deleteLastKElements(counter, cellsToColor);
+                break;
+            }
+        }
+        for (Cell item : cellsToColor) {
+            item.changeColor(playerColor);
+        }
+    }
+    private void colorDiagonals(Cell[][] _fieldArray, int i, int j) {
+        Colors playerColor = _fieldArray[i][j].getColor();
+        ArrayList<Cell> cellsToColor = new ArrayList<Cell>();
+        int counter = 0;
+        for (int k = 1; (i - k) >= 0 && (j + k) < 8; k++) {
+            if (!_fieldArray[i - k][j + k].isFree()) {
+                if (_fieldArray[i - k][j + k].getColor() != playerColor) {
+                    cellsToColor.add(_fieldArray[i - k][j + k]);
+                    ++counter;
+                    if ((i - k) == 0 && (j + k) == 7) { deleteLastKElements(counter, cellsToColor); }
+                } else {
+                    break;
+                }
+            } else {
+                deleteLastKElements(counter, cellsToColor);
+                break;
+            }
+        }
+        counter = 0;
+        for (int k = 1; (i + k) < 8 && (j + k) < 8; k++) {
+            if (!_fieldArray[i + k][j + k].isFree()) {
+                if (_fieldArray[i + k][j + k].getColor() != playerColor) {
+                    cellsToColor.add(_fieldArray[i + k][j + k]);
+                    ++counter;
+                    if ((i + k) == 7 && (j + k) == 7) { deleteLastKElements(counter, cellsToColor); }
+                } else {
+                    break;
+                }
+            } else {
+                deleteLastKElements(counter, cellsToColor);
+                break;
+            }
+        }
+        counter = 0;
+        for (int k = 1; (i + k) < 8 && (j - k) >= 0; k++) {
+            if (!_fieldArray[i + k][j - k].isFree()) {
+                if (_fieldArray[i + k][j - k].getColor() != playerColor) {
+                    cellsToColor.add(_fieldArray[i + k][j - k]);
+                    ++counter;
+                    if ((i - k) == 0 && (j - k) == 0) { deleteLastKElements(counter, cellsToColor); }
+                } else {
+                    break;
+                }
+            } else {
+                deleteLastKElements(counter, cellsToColor);
+                break;
+            }
+        }
+        counter = 0;
+        for (int k = 1; (i - k) >= 0 && (j - k) >= 0; k++) {
+            if (!_fieldArray[i - k][j - k].isFree()) {
+                if (_fieldArray[i - k][j - k].getColor() != playerColor) {
+                    cellsToColor.add(_fieldArray[i - k][j - k]);
+                    ++counter;
+                    if ((i - k) == 0 && (j - k) == 0) { deleteLastKElements(counter, cellsToColor); }
+                } else {
+                    break;
+                }
+            } else {
+                deleteLastKElements(counter, cellsToColor);
+                break;
+            }
+        }
+        for (Cell item : cellsToColor) {
+            item.changeColor(playerColor);
+        }
+    }
+    private boolean checkHorizontals(Cell[][] _fieldArray, Colors playerColor, int i, int j) {
+        boolean flag = false;
+        for (int n = i - 1; n >= 0; n--) {
+            if (!_fieldArray[n][j].isFree()) {
+                if (_fieldArray[n][j].getColor() != playerColor) {
+                    flag = true;
+                } else if (flag){
+                    return true;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        flag = false;
+        for (int n = i + 1; n < 8; n++) {
+            if (!_fieldArray[n][j].isFree()) {
+                if (_fieldArray[n][j].getColor() != playerColor) {
+                    flag = true;
+                } else if (flag){
+                    return true;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        flag = false;
+        for (int m = j - 1; m >= 0; m--) {
+            if (!_fieldArray[i][m].isFree()) {
+                if (_fieldArray[i][m].getColor() != playerColor) {
+                    flag = true;
+                } else if (flag){
+                    return true;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        flag = false;
+        for (int m = j + 1; m < 8; m++) {
+            if (!_fieldArray[i][m].isFree()) {
+                if (_fieldArray[i][m].getColor() != playerColor) {
+                    flag = true;
+                } else if (flag){
+                    return true;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        return false;
+    }
+    public boolean checkDiagonals(Cell[][] _fieldArray, Colors playerColor, int i, int j) {
+        Colors color = _fieldArray[i][j].getColor();
+        boolean flag = false;
+        for (int k = 1; (i - k) >= 0 && (j + k) < 8; k++) {
+            if (!_fieldArray[i - k][j + k].isFree()) {
+                if (_fieldArray[i - k][j + k].getColor() != playerColor) {
+                    flag = true;
+                } else if (flag){
+                    return true;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        flag = false;
+        for (int k = 1; (i + k) < 8 && (j + k) < 8; k++) {
+            if (!_fieldArray[i + k][j + k].isFree()) {
+                if (_fieldArray[i + k][j + k].getColor() != playerColor) {
+                    flag = true;
+                } else if (flag){
+                    return true;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        flag = false;
+        for (int k = 1; (i + k) < 8 && (j - k) >= 0; k++) {
+            if (!_fieldArray[i + k][j - k].isFree()) {
+                if (_fieldArray[i + k][j - k].getColor() != playerColor) {
+                    flag = true;
+                } else if (flag){
+                    return true;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        flag = false;
+        for (int k = 1; (i - k) >= 0 && (j - k) >= 0; k++) {
+            if (!_fieldArray[i - k][j - k].isFree()) {
+                if (_fieldArray[i - k][j - k].getColor() != playerColor) {
+                    flag = true;
+                } else if (flag){
+                    return true;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        return false;
+    }
+    public Finder() {};
+    public ArrayList<Cell> findAvailableCells(Cell[][] _fieldArray, Colors playerColor) {
+        ArrayList<Cell> availableCells = new ArrayList<Cell>();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (_fieldArray[i][j].isFree()
+                        && (checkHorizontals(_fieldArray, playerColor, i , j)
+                        || checkDiagonals(_fieldArray, playerColor, i, j))) {
+                    availableCells.add(_fieldArray[i][j]);
+                    _fieldArray[i][j].isAvailable = true;
+                } else {
+                    _fieldArray[i][j].isAvailable = false;
+                }
+            }
+        }
+        return availableCells;
+    }
+    public void colorAllEatenCells(Cell[][] _fieldArray, int i, int j) {
+        colorHorizontals(_fieldArray, i, j);
+        colorDiagonals(_fieldArray, i, j);
+    }
+}
